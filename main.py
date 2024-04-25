8824c94e3a456d41da8d3eec0a7c3d2d393c15f8
Hannan Javed <hannansahib@gmail.com>
2024-04-25 18:05:14 +0800

    changed download directory

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-# Import libraries
 import requests, re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time, os
+import os
 
-# downloads folder
-download_directory = "C:\\Users\\hanna\\Downloads"
+def get_download_directory():
+    home_directory = os.path.expanduser("~")  # Get user's home directory
+    
+    # Check the operating system to determine the download directory
+    if os.name == 'posix':  # Linux or macOS
+        download_directory = os.path.join(home_directory, 'Downloads')
+    elif os.name == 'nt':  # Windows
+        download_directory = os.path.join(home_directory, 'Downloads')
+    else:
+        download_directory = None  # Unsupported operating system
+    
+    return download_directory
+
+download_directory = get_download_directory()
 
 def clear_undownloaded_files():
     # get all the files
@@ -31,7 +43,7 @@ def download_episode(i):
         totaltime+=5
         files = os.listdir(download_directory)
     # wait two and a half minutes before returning false
-    if totaltime > 150:
+    if totaltime > 30:
         return False
     return True
 
@@ -49,16 +61,12 @@ def download_episodes(url, start_episode, end_episode):
         # find episode download page id
         id = re.findall("id=[0-9A-Za-z]*",str(videosource_link[0]))[0]
         downloadpagelink = "https://embtaku.pro/download?"+id+"&title=Psycho-Pass+3+Episode+"+str(i)+"&typesub=SUB&mip=0.0.0.0&refer=https://goone.pro/&ch=d41d8cd98f00b204e9800998ecf8427e&token2=4J8hKUzBdsRuuak9VRDseg&expires2=1713795921&op=1"
-
-        # start simulating chrome
+    
         driver = webdriver.Chrome()
         driver.get(downloadpagelink)
-
-        # wait for page to load
-        time.sleep(3)
-        
         soup = BeautifulSoup(driver.page_source, "html.parser")
         downloadlinks = []
+
         # find links for all 1inks - 1080p, 720p, 480p and 360p
         for j in range(4):
             downloadlinks.append([soup.find_all('a')[j+1].text] + [re.findall("https[A-Za-z0-9-=:/.?]*",str(soup.find_all('a')[j+1]))[0]])
@@ -74,6 +82,15 @@ def download_episodes(url, start_episode, end_episode):
                 if ")" not in downloadlinks[-1][0]:
                     downloadlinks.pop()
                     continue
+
+                # start driver again
+                driver.quit()
+                 # start simulating chrome
+                driver = webdriver.Chrome()
+                driver.get(downloadpagelink)
+
+                # wait for page to load
+                time.sleep(3)
 
                 # clean directory by deleting undownloaded files
                 clear_undownloaded_files()
@@ -105,6 +122,7 @@ m - From episode 1 until episode m
 m,n - From episode m to n (m < n)
 m,-1 - From episode m to final
 All - From episode 1 until final episode
+(Just enter 1 if its a movie)
 ''')
     
     if episodes.lower() == 'all':
