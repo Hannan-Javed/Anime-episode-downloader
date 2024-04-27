7f48a21b4a289b8ee31de02b469efca066aa9ea3
Hannan Javed <hannansahib@gmail.com>
2024-04-27 21:26:44 +0800

    Fix title and some bug fixes

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Anime-episode-downloader
-This is the python implementation of downloading anime episodes from the website https://goone.pro automatically using selenium, which is a python libary used to simulate chrome
+This is the python implementation of downloading anime episodes from the website https://goone.pro automatically using selenium, which is a python libary used to simulate chrome.
+The episodes are downloaded in the download folder.
 ## Installation
 First download the required packages
 ```shell
 pip install -r requirements.txt
 ```
+## Requirements 
+Make sure that you are not downloading any other file in the download folder
 ## Run
 Run `main.py` and input two things:
 1. url: Go to the website https://goone.pro and search for the anime you want to download. Go the **first episode** of that anime and copy the URL, and paste it in the terminal.
@@ -15,3 +18,5 @@ m,-1 - From episode m to final
 All - From episode 1 until final episode
 
 After inputting and clicking enter, the downloading will automatically start and it will try to download from the highest quality. If theres an error in the link, it will try the lower quality links if they work. Episode will not be downloaded if it takes longer than **Two and a half minutes** to download or **No links work**.
+## Note
+Running this will delete any undownloaded files in the downloads folder
--- main.py
+++ main.py
@@ -37,95 +37,121 @@ def download_episode(i):
     if ".crdownload" in "".join(files) == False:
         # Episode didnt start downloading
         return False
-    while(".crdownload" in "".join(files)) and totaltime<=150:
-        print("Downloading episode "+str(i)+".......")
-        time.sleep(5)
-        totaltime+=5
+    while (".crdownload" in "".join(files)) and totaltime<150:
+        time.sleep(1)
+        totaltime+=1
+        # print every five seconds
+        if totaltime%5==0:
+            print("Downloading episode "+str(i)+".......")
         files = os.listdir(download_directory)
     # wait two and a half minutes before returning false
-    if totaltime > 30:
-        return False
-    return True
+    return totaltime<150
 
 def download_episodes(url, start_episode, end_episode):
 
     i = start_episode
     if end_episode == -1:
         end_episode = 10000
+    response = requests.get(url+str(i))
+    soup = BeautifulSoup(response.text, 'html.parser')
+    videosource_link = soup.findAll('iframe')
+    if not videosource_link:
+        print("Cannot find download link for episode "+str(i))
+        i+=1
+    # title is fixed so find outside loop
+    title = "&"+re.findall("title=[A-Za-z+]*",str(videosource_link[0]))[0]
+
     while i < end_episode+1:
-        response = requests.get(url+str(i))
-        soup = BeautifulSoup(response.text, 'html.parser')
-        videosource_link = soup.findAll('iframe')
-        if not videosource_link:
-            break
+        
         # find episode download page id
         id = re.findall("id=[0-9A-Za-z]*",str(videosource_link[0]))[0]
-        downloadpagelink = "https://embtaku.pro/download?"+id+"&title=Psycho-Pass+3+Episode+"+str(i)+"&typesub=SUB&mip=0.0.0.0&refer=https://goone.pro/&ch=d41d8cd98f00b204e9800998ecf8427e&token2=4J8hKUzBdsRuuak9VRDseg&expires2=1713795921&op=1"
-    
-        driver = webdriver.Chrome()
-        driver.get(downloadpagelink)
-        soup = BeautifulSoup(driver.page_source, "html.parser")
+        downloadpagelink = "https://embtaku.pro/download?"+id+title+str(i)+"&typesub=SUB"
         downloadlinks = []
 
+        # start simulating chrome
+        driver = webdriver.Chrome()
+        driver.get(downloadpagelink)
+
+        # wait for page to load
+        time.sleep(3)
+        
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
         # find links for all 1inks - 1080p, 720p, 480p and 360p
         for j in range(4):
             downloadlinks.append([soup.find_all('a')[j+1].text] + [re.findall("https[A-Za-z0-9-=:/.?]*",str(soup.find_all('a')[j+1]))[0]])
         
-        successful = False
-        while len(downloadlinks)!=0:
+        # clean directory by deleting undownloaded files
+        clear_undownloaded_files()
 
-            if successful:
-                print("Successfully downloaded episode "+str(i)+"!")
+        # click on the link in reverse order (e.g. 1080p is the last link)
+        downloadlink = driver.find_element(By.XPATH,'//a[@href="'+downloadlinks[-1][1]+'"]')
+        driver.execute_script("arguments[0].click();", downloadlink)
+        time.sleep(2)
+
+        successful = download_episode(i)
+        # if unsuccessful, try other links
+        while not successful and len(downloadlinks)!=0:
+            # pop the last link as it was unsuccessful
+            downloadlinks.pop()
+            # check if its one of 1080p, 720p, 480p or 360p link
+            while ")" not in downloadlinks[-1][0]:
+                downloadlinks.pop()
+                continue
+            # if no link is valid exit loop
+            if len(downloadlinks)==0:
                 break
-            else:
-                # check if its one of 1080p, 720p, 480p or 360p link
-                if ")" not in downloadlinks[-1][0]:
-                    downloadlinks.pop()
-                    continue
+            # start driver again
+            driver.quit()
+            # start simulating chrome
+            driver = webdriver.Chrome()
+            driver.get(downloadpagelink)
 
-                # start driver again
-                driver.quit()
-                 # start simulating chrome
-                driver = webdriver.Chrome()
-                driver.get(downloadpagelink)
+            # wait for page to load
+            time.sleep(3)
 
-                # wait for page to load
-                time.sleep(3)
+            # clean directory by deleting undownloaded files
+            clear_undownloaded_files()
+            
+            # click on the link in reverse order (e.g. 1080p is the last link)
+            downloadlink = driver.find_element(By.XPATH,'//a[@href="'+downloadlinks[-1][1]+'"]')
+            driver.execute_script("arguments[0].click();", downloadlink)
+            time.sleep(2)
 
-                # clean directory by deleting undownloaded files
-                clear_undownloaded_files()
-
-                # click on the link in reverse order (e.g. 1080p is the last link)
-                downloadlink = driver.find_element(By.XPATH,'//a[@href="'+downloadlinks[-1][1]+'"]')
-                driver.execute_script("arguments[0].click();", downloadlink)
-                time.sleep(2)
-
-                successful = download_episode(i)
+            successful = download_episode(i)
         
-        # cannot download from any link
-        if len(downloadlinks)==0:
+        if successful:
+            print("Successfully downloaded episode "+str(i)+"!")
+        else:
+            # cannot download from any link
             print("Error! Cannot downloaded episode "+str(i))
         driver.quit()
+        
+        # go to next page
         i+=1
+        response = requests.get(url+str(i))
+        soup = BeautifulSoup(response.text, 'html.parser')
+        videosource_link = soup.findAll('iframe')
+        if not videosource_link:
+            print("Cannot find download link for episode "+str(i))
+            continue
     print("All episodes downloaded!")
             
 
 if __name__ == "__main__":
 
-    url = input('''Go to this website and paste the link of the first episode of the anime you would like to download:
+    url = re.findall("[A-Za-z-:/?.+&]*",input('''Go to this website and paste the link of any episode of the anime you would like to download:
 https://goone.pro
-''')
-    url = url[:-1]
-    
+'''))[0]
+    print(url)
     episodes = input('''Enter the number of episodes you want to download
 m - From episode 1 until episode m
 m,n - From episode m to n (m < n)
 m,-1 - From episode m to final
 All - From episode 1 until final episode
-(Just enter 1 if its a movie)
+(Enter 1 if its a movie)
 ''')
     
-    if episodes.lower() == 'all':
+    if episodes.lower()[0] == 'a':
         download_episodes(url, 1, -1)
     elif len(episodes.split(','))>1:
         download_episodes(url, int(episodes.split(',')[0]), int(episodes.split(',')[1]))
