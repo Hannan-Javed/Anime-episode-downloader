7913b9599d0f58e7f66a2891afc67e1bd8dbd78c
Hannan-Javed <hannansahib@gmail.com>
2024-12-25 01:26:14 +0800

    remove unused import

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests, re, time, os
 from bs4 import BeautifulSoup
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Chrome, ChromeOptions
 
