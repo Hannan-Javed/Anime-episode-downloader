cd8bf4349bf6624f13b63d629eedeab082e1bf32
Hannan-Javed <hannansahib@gmail.com>
2025-01-11 14:39:08 +0500

    create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+from utils import get_default_download_directory
+
+download_directory = get_default_download_directory() # "/path/to/download/directory"
+time_limit = 2
+episode_type = "SUB" 
+invalid_filename_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
