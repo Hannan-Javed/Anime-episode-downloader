a81e5caf5c40c68ba4119162fee1305bd89c391c
Hannan-Javed <hannansahib@gmail.com>
2025-01-11 15:55:11 +0500

    change default time to 2 minutes 30 seconds

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from utils import get_default_download_directory
 
 download_directory = get_default_download_directory() # "/path/to/download/directory"
-time_limit = 2
+time_limit = 150
 episode_type = "SUB" 
 invalid_filename_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
