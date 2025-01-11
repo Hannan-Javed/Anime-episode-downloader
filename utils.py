c3fe1b50bd6d3e4e704c359c44a581ab64d06f28
Hannan-Javed <hannansahib@gmail.com>
2025-01-11 15:09:08 +0500

    make wrapper function for loading animation

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
-import os
+import os, threading, sys, time
 from PyInquirer import prompt
+from functools import wraps
 
 def get_default_download_directory():
     home_directory = os.path.expanduser("~")  # Get user's home directory
@@ -26,3 +27,28 @@ def list_menu_selector(qprompt, anime_list):
             ]
         )
     return menu['name']
+
+def loading_animation(message, stop_event):
+    while not stop_event.is_set():
+        for dots in range(4):  # 0 to 3 dots
+            sys.stdout.write("\r" + message + " " * 4) # clear previous dots
+            sys.stdout.flush()
+            sys.stdout.write("\r" + message + "." * dots)
+            sys.stdout.flush()
+            time.sleep(0.5)
+    print()
+
+def with_loading_animation(message):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            stop_event = threading.Event()
+            animation_thread = threading.Thread(target=loading_animation, args=(message, stop_event), daemon=True)
+            animation_thread.start()
+            try:
+                return func(*args, **kwargs)
+            finally:
+                stop_event.set()
+                animation_thread.join()
+        return wrapper  
+    return decorator
